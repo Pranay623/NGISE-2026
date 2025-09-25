a3f29074f0be9a6fa093a2c8346b474d55a3a459
piyushkrsingh07 <piyush23153101@akgec.ac.in>
2025-09-25 21:52:06 +0530

    created hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link, useLocation } from "react-router-dom";
 import Logo from "../public/main logo.png";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
 // import IEEE from "../public/IEEE.png"; // if you decide to use again
 
 export default function Navbar() {
   const location = useLocation();
+  const [isSheetOpen,setIsSheetOpen]=useState(false)
 
   // Function to handle active link styling
   const linkClass = (path: string) =>
@@ -32,7 +35,7 @@ export default function Navbar() {
           />
 
           {/* Navigation Links */}
-          <div className="hidden md:flex items-center space-x-8">
+          <div className="hidden lg:flex items-center space-x-8">
             <Link to="/" className={linkClass("/")}>
               Home
             </Link>
@@ -62,6 +65,56 @@ export default function Navbar() {
             </Link>
           </div>
 
+      <div className='lg:hidden mb-4'>
+        <div className='flex items-center'>
+
+            <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+  <SheetTrigger asChild>
+    <Button variant="outline" className="flex items-center gap-2 mt-2">
+        <img src="https://ucarecdn.com/f601cf8c-0502-43aa-810b-72542ba282c3/-/preview/1000x1000/" className="w-5 h-5"/>
+      
+    </Button>
+  </SheetTrigger>
+  <SheetContent side="top" className="w-full overflow-y-auto">
+    <SheetHeader>
+ 
+      
+    </SheetHeader>
+
+    <div className='p-6 flex flex-col gap-y-5 text-center ' onClick={()=>setIsSheetOpen(false)}>
+         <Link to="/" >
+              Home
+            </Link>
+            <Link to="/venue" >
+              Venue
+            </Link>
+            <Link to="/call-for-papers" >
+              Call for Papers
+            </Link>
+            <Link to="/speakers" >
+              Speakers
+            </Link>
+            <Link to="/committee" >
+              Committee
+            </Link>
+            <Link to="/program" >
+              Program
+            </Link>
+            <Link to="/registrations" >
+              Registration
+            </Link>
+            <Link to="/locale" >
+              Locale
+            </Link>
+            <Link to="/venue" >
+              Contact Us
+            </Link>
+    </div>
+
+  </SheetContent>
+</Sheet>
+        </div>
+      </div>
           {/* Right Logo (optional) */}
           {/* <motion.img
             src={IEEE}
