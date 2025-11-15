cde2424ed697be0446ca2f0461c1ebd896ba5199
Pranay623 <srivastavapranay623@gmail.com>
2025-11-15 09:52:52 +0530

    FIX: AKGEC text for mobile view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -129,7 +129,8 @@ export default function Hero() {
   /> */}
   <motion.div className="flex items-center space-x-2" whileHover={{ scale: 1.05 }}>
     <MapPin className="w-5 h-5 text-blue-400" />
-    <span>Ajay Kumar Garg Engineering College, Ghaziabad, India</span>
+    <span className="hidden sm:inline">Ajay Kumar Garg Engineering College, Ghaziabad, India</span>
+    <span className="sm:hidden bold">AKGEC,Ghaziabad,India</span>
   </motion.div>
 </motion.div>
 
