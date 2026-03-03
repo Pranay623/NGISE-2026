f8745d81fa738b5e2c2dc23f68a4b81868a6f25f
anjumishra80 <80882414+anjumishra80@users.noreply.github.com>
2026-03-03 15:42:57 +0530

    Update TechnicalOrgannizers.tsx

--- src/pages/TechnicalOrgannizers.tsx
+++ src/pages/TechnicalOrgannizers.tsx
@@ -32,6 +32,7 @@ export default function TechnicalOrgannizers() {
                         <h4 className="text-xl font-bold text-gray-700 mb-6 text-center">Program Committee Chairs</h4>
                         <div className="p-6 bg-white shadow-sm rounded-xl border border-gray-100 text-left">
                             {/*<p className="font-semibold text-gray-900 text-lg">Markus Bertl</p>*/}
+                            <p className="text-sm">Markus Bertl, Vienna University of Economics, Austria</p>
                             <p className="text-sm">Dirk Draheim, Tallinn University of Technology, Estonia</p>
                             <p className="text-sm">Minakshi Kaushik, Karlsruhe Institute of Technology, Germany</p>
                             <p className="text-sm">Silvia Lips, Tallinn University of Technology, Estonia</p>
