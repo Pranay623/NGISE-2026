84b3d92e44b076f8d41f357b423b2d32b79c5c3e
anjumishra80 <80882414+anjumishra80@users.noreply.github.com>
2026-03-03 15:29:00 +0530

    Update TechnicalOrgannizers.tsx

--- src/pages/TechnicalOrgannizers.tsx
+++ src/pages/TechnicalOrgannizers.tsx
@@ -29,10 +29,13 @@ export default function TechnicalOrgannizers() {
                     </div> */}
                     {/* Technical Program Committee Co-Chair */}
                     <div className="max-w-4xl mx-auto mb-10">
-                        <h4 className="text-xl font-bold text-gray-700 mb-6 text-center">Program Committee Chair</h4>
-                        <div className="p-6 bg-white shadow-sm rounded-xl border border-gray-100 text-center">
-                            <p className="font-semibold text-gray-900 text-lg">Dr. Anupama Sharma</p>
-                            <p className="text-gray-500 text-sm">Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+                        <h4 className="text-xl font-bold text-gray-700 mb-6 text-center">Program Committee Chairs</h4>
+                        <div className="p-6 bg-white shadow-sm rounded-xl border border-gray-100 text-left">
+                            {/*<p className="font-semibold text-gray-900 text-lg">Markus Bertl</p>*/}
+                            <p className="text-gray-500 text-sm">Dirk Draheim, Tallinn University of Technology, Estonia</p>
+                            <p className="text-gray-500 text-sm">Minakshi Kaushik, Karlsruhe Institute of Technology, Germany</p>
+                            <p className="text-gray-500 text-sm">Silvia Lips, Tallinn University of Technology, Estonia</p>
+                            <p className="text-gray-500 text-sm">Rahul Sharma, Karlsruhe Institute of Technology, Germany</p>
                         </div>
                     </div>
 
