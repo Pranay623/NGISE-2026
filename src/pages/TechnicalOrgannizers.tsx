149b48556acd70e9561d63a152b986301662d333
anjumishra80 <80882414+anjumishra80@users.noreply.github.com>
2026-03-03 17:21:03 +0530

    Update TechnicalOrgannizers.tsx

--- src/pages/TechnicalOrgannizers.tsx
+++ src/pages/TechnicalOrgannizers.tsx
@@ -42,10 +42,36 @@ export default function TechnicalOrgannizers() {
 
                     {/* Technical Program Committee */}
                     <h4 className="text-xl font-bold text-gray-700 mb-6 text-center">Program Committee</h4>
-                    <div className="max-w-4xl mx-auto py-8 bg-white shadow-sm rounded-xl border border-gray-100">
+                    <div className="max-w-4xl mx-auto py-8 bg-white shadow-sm rounded-xl border border-gray-100 text-left">
                          <p className="text-base">Rozha Kamal Ahmed, Sulaimani Polytechnic University, Iraq</p>
                          <p className="text-base">Colin Atkinson, Software Engineering Group, University of Mannheim, Germany</p>
-                        
+                         <p className="text-base">Edward Bernroider, WU Wien, Vienna Uiversity of Economics, Austria</p>
+                         <p className="text-base">Sidra Azmat Butt, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Tania Cerquitelli, Politecnico di Torino, Italy</p>
+                         <p className="text-base">Gillian Dobbie, Computer Science, University of Auckland, New Zealand</p>
+                         <p className="text-base">Dirk Draheim, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Vipin Deval, KIET Group, Ghaziabad, India</p>
+                         <p className="text-base">Vimal Dwivedi, University of Ulster, UK</p>
+                         <p className="text-base">Lisa Ehrlinger, Hasso Plattner Institute, Germany</p>
+                         <p className="text-base">Iztok Fister, University of Maribor, Slovenia</p>
+                         <p className="text-base">Verena Geist, SCCH (Software Competence Center Hagenberg), Hagenberg, Austria</p>
+                         <p className="text-base">Minakshi Kaushik, Karlsruhe Institute of Technology, Germany</p>
+                         <p className="text-base">Innar Liiv, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Silvia Lips, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Olaf Maennel, University of Adeleide, Australia</p>
+                         <p className="text-base">Takehiko Nagumo, Kyoto University, Japan</p>
+                         <p className="text-base">Anastasija Nikiforova, University of Tartu, Estonia</p>
+                         <p className="text-base">Ingrid Pappel, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Vishwajeet Pattanaik, Indian Institute of Science, Bengalore, India</p>
+                         <p className="text-base">Sijo Arakkal Peious, Tallinn University of Technology, Estonia</p>
+                         <p className="text-base">Arun Kumar Sangaiah, National Yunlin University of Science and Technology, Taiwan</p>
+                         <p className="text-base">Rahul Sharma, Karlsruhe Institute of Technology, Germany</p>
+                         <p className="text-base">Attique Shah, City University of Birminghem, UK</p>
+                         <p className="text-base">Divesh Srivastava, AT&T Labs Research, USA</p>
+                         <p className="text-base">Shweta Suran, Max-Planck Institute, Berlin, Germany</p>
+                         <p className="text-base">Kuldar Taveter, University of Tartu, Estonia</p>
+                         <p className="text-base">A Min Tjoa, Technical University Vienna, Austria</p>
+                         <p className="text-base">Thomas Ziebermayer, University of Applied Sciences Upper Austria, Hagenberg, Austria</p>
                         {/*<ul className="grid grid-cols-1 gap-y-0.5 px-8">
                             {[
                                 "Rozha Kamal Ahmed, Sulaimani Polytechnic University, Iraq",
