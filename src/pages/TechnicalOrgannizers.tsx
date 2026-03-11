f7a98dfe10082d6419462765d93fb6854e94f773
Nikhil <nikhilgupta542006@gmail.com>
2026-03-11 20:16:58 +0530

    program community ui

--- src/pages/TechnicalOrgannizers.tsx
+++ src/pages/TechnicalOrgannizers.tsx
@@ -38,107 +38,94 @@ export default function TechnicalOrgannizers() {
 
                     {/* Technical Program Committee */}
                     {/*<h4 className="text-xl font-bold text-gray-700 mb-6 text-center">Program Committee</h4>*/}
-                    <div className="max-w-4xl mx-auto py-8 bg-white shadow-sm rounded-xl border border-gray-100 text-left">
-                        {/*<p className="text-base">Rozha Kamal Ahmed, Sulaimani Polytechnic University, Iraq</p>
-                         <p className="text-base">Colin Atkinson, Software Engineering Group, University of Mannheim, Germany</p>
-                         <p className="text-base">Edward Bernroider, WU Wien, Vienna Uiversity of Economics, Austria</p>
-                         <p className="text-base">Sidra Azmat Butt, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Tania Cerquitelli, Politecnico di Torino, Italy</p>
-                         <p className="text-base">Gillian Dobbie, Computer Science, University of Auckland, New Zealand</p>
-                         <p className="text-base">Dirk Draheim, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Vipin Deval, KIET Group, Ghaziabad, India</p>
-                         <p className="text-base">Vimal Dwivedi, University of Ulster, UK</p>
-                         <p className="text-base">Lisa Ehrlinger, Hasso Plattner Institute, Germany</p>
-                         <p className="text-base">Iztok Fister, University of Maribor, Slovenia</p>
-                         <p className="text-base">Verena Geist, SCCH (Software Competence Center Hagenberg), Hagenberg, Austria</p>
-                         <p className="text-base">Minakshi Kaushik, Karlsruhe Institute of Technology, Germany</p>
-                         <p className="text-base">Innar Liiv, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Silvia Lips, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Olaf Maennel, University of Adeleide, Australia</p>
-                         <p className="text-base">Takehiko Nagumo, Kyoto University, Japan</p>
-                         <p className="text-base">Anastasija Nikiforova, University of Tartu, Estonia</p>
-                         <p className="text-base">Ingrid Pappel, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Vishwajeet Pattanaik, Indian Institute of Science, Bengalore, India</p>
-                         <p className="text-base">Sijo Arakkal Peious, Tallinn University of Technology, Estonia</p>
-                         <p className="text-base">Arun Kumar Sangaiah, National Yunlin University of Science and Technology, Taiwan</p>
-                         <p className="text-base">Rahul Sharma, Karlsruhe Institute of Technology, Germany</p>
-                         <p className="text-base">Attique Shah, City University of Birminghem, UK</p>
-                         <p className="text-base">Divesh Srivastava, AT&T Labs Research, USA</p>
-                         <p className="text-base">Shweta Suran, Max-Planck Institute, Berlin, Germany</p>
-                         <p className="text-base">Kuldar Taveter, University of Tartu, Estonia</p>
-                         <p className="text-base">A Min Tjoa, Technical University Vienna, Austria</p>
-                         <p className="text-base">Thomas Ziebermayer, University of Applied Sciences Upper Austria, Hagenberg, Austria</p>*/}
-                        <ul className="grid grid-cols-1 gap-y-0.5 px-8">
-                            {[
-                                "Rozha Kamal Ahmed, Sulaimani Polytechnic University, Iraq",
-                                "Colin Atkinson, Software Engineering Group, University of Mannheim, Germany",
-                                "Ladjel Bellatreche, University of Poitiers, France",
-                                "Sadok Ben Yahia, University of Southern Denmark, Denmark",
-                                "Edward Bernroider, WU Wien, Vienna Uiversity of Economics, Austria",
-                                "Markus Bertl (PC Chair), Vienna University of Economics, Austria",
-                                "Rakshya Bhattarai, Hanken School of Economics, Helsinki, Finland",
-                                "Ahto Buldas, Tallinn University of Technology, Estonia",
-                                "Sidra Azmat  Butt, Tallinn University of Technology, Estonia",
-                                "Tania Cerquitelli, Politecnico di Torino, Italy",
-                                "Vishal Chaudhary, Tallinn University of Technology, Estonia",
-                                "Tanmoy  Deb , Motherson Innovation, Noida, India",
-                                "Vipin Deval, KIET Group, Ghaziabad, India",
-                                "Gillian Dobbie, Computer Science, University of Auckland, New Zealand",
-                                "Dirk Draheim (PC Chair), Tallinn University of Technology, Estonia",
-                                "Vimal Dwivedi, University of Ulster , UK",
-                                "Lisa Ehrlinger, Hasso Plattner Institute, Potsdam, Germany",
-                                "Kevin Feichtinger, Karlsruhe Institute of Technology, Germany",
-                                "Iztok Fister, University of Maribor, Slovenia",
-                                "Teodoro Forcht-Dagi, Queen’s University Belfast, UK",
-                                "Anchal Garg, University of Greater Manchester, Bolton, UK",
-                                "Verena Geist, SCCH (Software Competence Center Hagenberg), Austria",
-                                "Shilpi Gupta, University of Mannheim, Germany",
-                                "Ruchi Gupta, Ajay Kumar Garg Engineering College, Ghaziabad, India",
-                                "Eric Jackson, Tallinn University of Technology, Estonia",
-                                "Tina Jukic, University of Ljubljana, Slovenia",
-                                "Minakshi Kaushik (PC Chair), Karlsruhe Institute of Technology, Germany",
-                                "Ismail Khalil, Johannes Kepler University Linz, Austria",
-                                "Naho Kitano, Asukoe Partners, Tokio, Japan",
-                                "Dieter Kranzlmüller, Ludwig Maximilian University, Munich, Germany",
-                                "Josef Küng, Johannes Kepler University Linz, Austria",
-                                "Atul  Kumar Srivastava, Amity University Tashkent, Uzbekistan",
-                                "Punam  Kumari, Amity University Bengaluru, India",
-                                "Arne Lange, Karlsruhe Institute of Technology, Germany",
-                                "Janika Leoste, Tallinn University of Technology, Estonia",
-                                "Innar Liiv, Tallinn University of Technology, Estonia",
-                                "Silvia Lips (PC Chair), Tallinn University of Technology, Estonia",
-                                "Olaf Maennel, University of Adeleide, Australia",
-                                "Anju Mishra, Ajay Kumar Garg Engineering College, Ghaziabad, India",
-                                "Takehiko Nagumo, Kyoto University, Japan",
-                                "Felix Naumann, Hasso Plattner Institute, Potsdam, Germany",
-                                "Anastasija Nikiforova, University of Tartu, Estonia",
-                                "Alexander Norta, University of Pretoria , South Africa",
-                                "Monalisha  Ojha, University of Mannheim, Germany",
-                                "Ingrid Pappel, Tallinn University of Technology, Estonia",
-                                "Vishwajeet Pattanaik, Indian Institute of Science, Bengalore, India",
-                                "Sijo Arakkal Peious, Tallinn University of Technology, Estonia",
-                                "Deepak Rai, National Institute of Technology Jamshedpur, India",
-                                "Arun Kumar Sangaiah, National Yunlin University of Science and Technology, Taiwan",
-                                "Shelly Sachdeva, National Institute of Technology Delhi, India",
-                                "Dolly Sharma, Shiv Nadar University, Greater Noida, India",
-                                "Attique Shah, City University of Birmingham, UK",
-                                "Rahul Sharma (PC Chair), Karlsruhe Institute of Technology, Germany",
-                                "Anupama Sharma, Ajay Kumar Garg Engineering College, Ghaziabad, India",
-                                "Gaurav  Singal, Netaji Subhas University of Technology, India",
-                                "Akanksha  Singh, Benett University, Greater Noida, India",
-                                "Il-Yeol Song, Drexel University, Philadelphia, USA",
-                                "Divesh Srivastava, AT&T Labs Research, USA",
-                                "Kuldar Taveter, University of Tartu, Estonia",
-                                "A Min Tjoa, Technical University Vienna, Austria",
-                                "Ankit Vidyarthi, Jaypee Institute of Information Technology Noida, India",
-                                "Weixing Zang, Karlsruhe Institute of Technology, Germany",
-                                "Thomas Ziebermayer, University of Applied Sciences Upper Austria, Hagenberg, Austria",
-                            ].map((member, idx) => (
-                                <li key={idx} className="py-1 text-left">
-                                    <span className="text-gray-700 font-medium text-sm sm:text-base">{member}</span>
-                                </li>
-                            ))}
-                        </ul>
+                    <div className="max-w-4xl mx-auto py-8 text-left">
+
+                        <div className="overflow-hidden rounded-2xl border border-gray-200 shadow-sm">
+                            <ul className="flex flex-col">
+                                {[
+                                    "Rozha Kamal Ahmed, Sulaimani Polytechnic University, Iraq",
+                                    "Colin Atkinson, Software Engineering Group, University of Mannheim, Germany",
+                                    "Ladjel Bellatreche, University of Poitiers, France",
+                                    "Sadok Ben Yahia, University of Southern Denmark, Denmark",
+                                    "Edward Bernroider, WU Wien, Vienna Uiversity of Economics, Austria",
+                                    "Markus Bertl (PC Chair), Vienna University of Economics, Austria",
+                                    "Rakshya Bhattarai, Hanken School of Economics, Helsinki, Finland",
+                                    "Ahto Buldas, Tallinn University of Technology, Estonia",
+                                    "Sidra Azmat  Butt, Tallinn University of Technology, Estonia",
+                                    "Tania Cerquitelli, Politecnico di Torino, Italy",
+                                    "Vishal Chaudhary, Tallinn University of Technology, Estonia",
+                                    "Tanmoy  Deb , Motherson Innovation, Noida, India",
+                                    "Vipin Deval, KIET Group, Ghaziabad, India",
+                                    "Gillian Dobbie, Computer Science, University of Auckland, New Zealand",
+                                    "Dirk Draheim (PC Chair), Tallinn University of Technology, Estonia",
+                                    "Vimal Dwivedi, University of Ulster , UK",
+                                    "Lisa Ehrlinger, Hasso Plattner Institute, Potsdam, Germany",
+                                    "Kevin Feichtinger, Karlsruhe Institute of Technology, Germany",
+                                    "Iztok Fister, University of Maribor, Slovenia",
+                                    "Teodoro Forcht-Dagi, Queen’s University Belfast, UK",
+                                    "Anchal Garg, University of Greater Manchester, Bolton, UK",
+                                    "Verena Geist, SCCH (Software Competence Center Hagenberg), Austria",
+                                    "Shilpi Gupta, University of Mannheim, Germany",
+                                    "Ruchi Gupta, Ajay Kumar Garg Engineering College, Ghaziabad, India",
+                                    "Eric Jackson, Tallinn University of Technology, Estonia",
+                                    "Tina Jukic, University of Ljubljana, Slovenia",
+                                    "Minakshi Kaushik (PC Chair), Karlsruhe Institute of Technology, Germany",
+                                    "Ismail Khalil, Johannes Kepler University Linz, Austria",
+                                    "Naho Kitano, Asukoe Partners, Tokio, Japan",
+                                    "Dieter Kranzlmüller, Ludwig Maximilian University, Munich, Germany",
+                                    "Josef Küng, Johannes Kepler University Linz, Austria",
+                                    "Atul  Kumar Srivastava, Amity University Tashkent, Uzbekistan",
+                                    "Punam  Kumari, Amity University Bengaluru, India",
+                                    "Arne Lange, Karlsruhe Institute of Technology, Germany",
+                                    "Janika Leoste, Tallinn University of Technology, Estonia",
+                                    "Innar Liiv, Tallinn University of Technology, Estonia",
+                                    "Silvia Lips (PC Chair), Tallinn University of Technology, Estonia",
+                                    "Olaf Maennel, University of Adeleide, Australia",
+                                    "Anju Mishra, Ajay Kumar Garg Engineering College, Ghaziabad, India",
+                                    "Takehiko Nagumo, Kyoto University, Japan",
+                                    "Felix Naumann, Hasso Plattner Institute, Potsdam, Germany",
+                                    "Anastasija Nikiforova, University of Tartu, Estonia",
+                                    "Alexander Norta, University of Pretoria , South Africa",
+                                    "Monalisha  Ojha, University of Mannheim, Germany",
+                                    "Ingrid Pappel, Tallinn University of Technology, Estonia",
+                                    "Vishwajeet Pattanaik, Indian Institute of Science, Bengalore, India",
+                                    "Sijo Arakkal Peious, Tallinn University of Technology, Estonia",
+                                    "Deepak Rai, National Institute of Technology Jamshedpur, India",
+                                    "Arun Kumar Sangaiah, National Yunlin University of Science and Technology, Taiwan",
+                                    "Shelly Sachdeva, National Institute of Technology Delhi, India",
+                                    "Dolly Sharma, Shiv Nadar University, Greater Noida, India",
+                                    "Attique Shah, City University of Birmingham, UK",
+                                    "Rahul Sharma (PC Chair), Karlsruhe Institute of Technology, Germany",
+                                    "Anupama Sharma, Ajay Kumar Garg Engineering College, Ghaziabad, India",
+                                    "Gaurav  Singal, Netaji Subhas University of Technology, India",
+                                    "Akanksha  Singh, Benett University, Greater Noida, India",
+                                    "Il-Yeol Song, Drexel University, Philadelphia, USA",
+                                    "Divesh Srivastava, AT&T Labs Research, USA",
+                                    "Kuldar Taveter, University of Tartu, Estonia",
+                                    "A Min Tjoa, Technical University Vienna, Austria",
+                                    "Ankit Vidyarthi, Jaypee Institute of Information Technology Noida, India",
+                                    "Weixing Zang, Karlsruhe Institute of Technology, Germany",
+                                    "Thomas Ziebermayer, University of Applied Sciences Upper Austria, Hagenberg, Austria",
+                                ].map((member, idx) => (
+                                    <motion.li
+                                        key={idx}
+                                        initial={{ opacity: 0, x: -10 }}
+                                        whileInView={{ opacity: 1, x: 0 }}
+                                        transition={{ delay: idx * 0.01 }}
+                                        viewport={{ once: true }}
+                                        className={`
+                                            p-4 px-8 transition-colors duration-200 cursor-default
+                                            ${idx % 2 === 0
+                                                ? "bg-white"
+                                                : "bg-gray-200"}
+                                            hover:bg-gray-100/50
+                                        `}
+                                    >
+                                        <span className="text-gray-700 font-medium text-sm sm:text-base tracking-tight">{member}</span>
+                                    </motion.li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </motion.div>
             </div>
