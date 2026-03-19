2cebe9eff33586cbc390b38d2dc8432291a73897
Nikhil <nikhilgupta542006@gmail.com>
2026-03-19 15:21:00 +0530

    Advisory community

--- src/pages/Organizers.tsx
+++ src/pages/Organizers.tsx
@@ -37,24 +37,7 @@ export default function Organizers() {
           </div>
 
 
-          {/* Conference Chairs */}
-          <h4 className="text-xl font-bold text-gray-700 mb-4 text-center">Conference Chairs</h4>
-          <div className="grid md:grid-cols-2 gap-6 mb-8">
-            <div className="p-4 bg-white shadow rounded-xl border border-gray-100">
-              <img src={drik} alt="Prof. (Dr.) Dirk Draheim" className="w-32 h-32 object-cover rounded-full mx-auto mb-4" />
-              <p className="font-medium text-center">Dirk Draheim</p>
-              <p className="text-gray-600 text-sm text-center">
-                Tallinn University of Technology, Estonia
-              </p>
-            </div>
-            <div className="p-4 bg-white shadow rounded-xl border border-gray-100">
-              <img src={rahul} alt="Rahul Sharma" className="w-32 h-32 object-cover object-right rounded-full mx-auto mb-4" />
-              <p className="font-medium text-center">Rahul Sharma</p>
-              <p className="text-gray-600 text-sm text-center">
-                Karlsruhe Institute of Technology, Germany
-              </p>
-            </div>
-          </div>
+        
 
           {/** Advisory Committee */}
           <h4 className="text-xl font-bold text-gray-700 mb-4 text-center">Advisory Committee</h4>
@@ -74,6 +57,26 @@ export default function Organizers() {
               </div>
             ))}
 
+          </div>
+
+
+            {/* Conference Chairs */}
+          <h4 className="text-xl font-bold text-gray-700 mb-4 text-center">Conference Chairs</h4>
+          <div className="grid md:grid-cols-2 gap-6 mb-8">
+            <div className="p-4 bg-white shadow rounded-xl border border-gray-100">
+              <img src={drik} alt="Prof. (Dr.) Dirk Draheim" className="w-32 h-32 object-cover rounded-full mx-auto mb-4" />
+              <p className="font-medium text-center">Dirk Draheim</p>
+              <p className="text-gray-600 text-sm text-center">
+                Tallinn University of Technology, Estonia
+              </p>
+            </div>
+            <div className="p-4 bg-white shadow rounded-xl border border-gray-100">
+              <img src={rahul} alt="Rahul Sharma" className="w-32 h-32 object-cover object-right rounded-full mx-auto mb-4" />
+              <p className="font-medium text-center">Rahul Sharma</p>
+              <p className="text-gray-600 text-sm text-center">
+                Karlsruhe Institute of Technology, Germany
+              </p>
+            </div>
           </div>
 
 
